1195b5dc47ef1bde1f628f49665f258620325766
lae0901 <stephenrichter@gmail.com>
2020-07-03 20:14:47 -0400

    parse_json

--- src/parse_json.ts
+++ src/parse_json.ts
@@ -7,6 +7,7 @@ type jsonItemType = 'object' | 'array' | 'scalar' | '' ;
 interface iJsonItem
 {
   start: number;
+  itemType: jsonItemType;
   obj?: iJsonObject;
   arr?: iJsonArray;
   scalar?: iJsonScalar;
@@ -24,7 +25,8 @@ interface iObjectProperty
   start: number;
   name: string;
   nameText: string;
-  value: iJsonItem;
+  item?: iJsonItem;
+  errmsg?: string;
 }
 
 interface iJsonArray
@@ -48,41 +50,129 @@ export function json_parse( text: string )
 {
   const xref_array = text_toLineXref(text) ;
   let start = 0 ;
-  const { valueType, fx } = parseItem( text, start ) ;
-  if ( valueType == 'object')
-  {
+  const item = parseItem( text, start ) ;
 
-  }
-  return 'abc' ;
+  return item ;
 }
 
 // ---------------------------------- parseItem ----------------------------------
-function parseItem( text: string, start: number ) : iJsonItem
+function parseItem( text: string, ix: number ) : iJsonItem
 {
   const re = /\s*([{\[\"])/g;
-  re.lastIndex = start ;
+  re.lastIndex = ix ;
   let fx = -1 ;
   const rv = re.exec(text) ;
-  let vt : valueType = '' ;
+  let itemType : jsonItemType = '' ;
+  let start = -1 ;
+  let obj : iJsonObject | undefined ;
   if ( rv &&  rv.length == 2 )
   {
-    fx = rv.index ;
+    start = rv.index ;
     const ch1 = rv[1] ;
     if ( ch1 == '{')
-      vt = 'object' ;
+    {
+      itemType = 'object' ;
+      obj = parseObject(text, start) ;
+    }
     else if ( ch1 == '[')
-      vt = 'array' ;
+    {
+      itemType = 'array' ;
+    }
     else if ( ch1 == '"')
-      vt = 'scalar' ;
+    {
+      itemType = 'scalar' ;
+    }
   }
-  return {valueType:vt, fx } ;
+  return { start, itemType, obj } ;
 }
 
 // ---------------------------------- parseObject ----------------------------------
-function parseObject( text: string, start: number)
+function parseObject( text: string, start: number) : iJsonObject
 {
   // object consists of series of "prop name": prop_value.  
-  // parse until the closing "}".
+  // parse properties until the closing "}".
+  let ix = start + 1 ;
+  let end = -1 ;
+  let errmsg = '' ;
+  const properties : iObjectProperty[] = [] ;
+  while(true)
+  {
+    // advance past whitespace to start of property name or close brace.
+    const re = /\s*(.)/g;
+    re.lastIndex = ix;
+    let fx = -1;
+    const rv = re.exec(text);
 
+    // nothing but whitespace
+    if ( !rv || rv.index != ix )
+      errmsg = 'missing end of object' ;
+    else
+    {
+      const ch1 = rv[1] ;
+      ix = ix + rv[0].length - 1 ;
 
+      if ( ch1 == '}')
+        break ;
+      else if ( ch1 == ',')
+        errmsg = 'unexpected delimiter' ;
+    }
+
+    // parse the object property
+    if ( !errmsg )
+    {
+      const prop = parseObjectProperty(text, ix) ;
+      properties.push(prop) ;
+    }
+    break ;
+  }
+
+  end = ix ;
+  return {start, end, properties } ;
+}
+
+// ---------------------------------- parseObjectProperty ----------------------------------
+function parseObjectProperty(text: string, start: number): iObjectProperty
+{
+  let ix = start;
+  let errmsg = '' ;
+  let isQuoted = false ;
+  let nameStart = -1 ;
+  let name = '' ;
+  let nameText = '' ;
+  let item : iJsonItem | undefined;
+
+  // start of property name. either double quoted or just a name. Followed by
+  // colon.
+  {
+    const re = /(\s*)(("([A-Za-z0-9_-]+)")|(\w+))\s*:/g;
+    re.lastIndex = ix;
+    let fx = -1;
+    const rv = re.exec(text);
+    if (!rv || rv.index != ix )
+      errmsg = 'expected property name not found' ;
+    else
+    {
+      nameStart = ix + rv[1].length ; // adv past whitespace to prop name.
+      if (rv[3])
+      {
+        nameText = rv[3] ;
+        name = rv[4] ;
+      }
+      else
+      {
+        nameText = rv[5] ;
+        name = rv[5] ;
+      }
+      ix = rv.index + rv[0].length;  // advance to past colon.
+    }
+  }
+
+  // got property name. now parse the item with is the property value.
+  // ( can be scalar, object or array )
+  if ( !errmsg )
+  {
+    item = parseItem(text, ix) ;
+  }
+
+  return { start:nameStart, name, nameText, item }
 }
