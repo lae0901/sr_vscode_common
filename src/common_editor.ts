2fc795691d08b845c9c38fd037dbdcd7b2632dda
lae0901 <stephenrichter@gmail.com>
2020-07-26 12:46:50 -0400

    activeEditor_selectionLine

--- src/common_editor.ts
+++ src/common_editor.ts
@@ -1,5 +1,22 @@
 import * as vscode from 'vscode';
 
+
+// -------------------------- activeEditor_selectionLine ------------------------
+// return the selection start TextLine of the active TextEditor.
+export function activeEditor_selectionLine( moreNumLines?: number )
+{
+  let line: vscode.TextLine | undefined;
+  const editor = vscode.window.activeTextEditor;
+  if (editor)
+  {
+    const doc = editor.document;
+    doc.getText() ;
+    line = doc.lineAt(editor.selection.start.line);
+  }
+  return line;
+}
+
+
 // -------------------------- editor_selectionLine ------------------------
 // return the selection start TextLine of the active TextEditor.
 export function editor_selectionLine( editor?: vscode.TextEditor  )
