b1eef08b86ec89ddbdaee1c1772103d0d9c50296
lae0901 <stephenrichter@gmail.com>
2021-10-16 22:39:28 -0400

    parse c. struct declare.

--- src/parse/parse_c.ts
+++ src/parse/parse_c.ts
@@ -31,3 +31,32 @@ export function c_declareFunctionName( text: string )
   return { returnType, returnType_pointerTo, funcName };
 }
 
+// --------------------------------- c_parseStruct ---------------------------------
+export function c_parseStruct( str:string )
+{
+
+}
+
+function c_parseStructDeclare_start( str:string)
+{
+  
+
+}
+
+// look for function name in form "returnType funcName( argtype argName )"
+{
+  // ( start of string or whitespace )
+  // ( return type name )
+  // ( optional * after type name or just whitespace )
+  // ( function name )
+  // ( open paren )
+  const regexp = /(^|\s+)(\w+)((\s*\*\s*)|\s+)(\w+)\s*(\()/;
+  const match = regexp.exec(text);
+  if (match)
+  {
+    returnType = match[2];
+    returnType_pointerTo = match[4] ? match[4].trim() : '';
+    funcName = match[5];
+  }
+}
+
